c5a9d06a174d1bc5bd8e42c3599efb964ac98f57
Taylor Laine <tlaine@msudenver.edu>
2017-05-02 01:43:12 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,7 +1,1 @@
-//Configuration for running tests and examples against the Twilio API
-module.exports = {
-    accountSid:'AC2882327b064e382326ada1db4d6b10d9',
-    authToken:'7a304082f2b7b9da03d6228b90e92e51',
-    from:'+17206339594', //The Twilio number you've bought or configured
-    to:'+17204963685' //The number you would like to send messages to for testing
-};
+
